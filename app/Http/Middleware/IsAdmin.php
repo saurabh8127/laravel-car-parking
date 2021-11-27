29ee9943428bc82db9d414639633b5b4c52492b1
InternIT <InternIT@hq.mbdgroup.com>
2021-11-27 16:55:43 +0530

    car parking

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+       if(!auth()->check() || 1 != auth()->user()->role){
+            // Auth::logout();
+
+            return redirect()->route('VerificationController.accessDenied');
+        }
+        return $next($request);
+    }
+}
---
+++ database/migrations/2021_11_27_075809_create_roles_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+        });
+
+        // Create Two Roles by Default
+        DB::table('roles')->insert([
+            [
+                'id' => 1,
+                'name' => 'admin',
+            ],
+            [
+                'id' => 2,
+                'name' => 'user',
+            ]
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
